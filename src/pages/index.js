0486a2c186c5d4b555692fbd50fce7d6e6937574
xu-chuan <xuchuan0112@qq.com>
2023-03-15 13:51:11 +0800

    skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,29 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: <>React</>,
+    imageUrl: 'img/react.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A JavaScript library for building user interfaces. Declarative, Component-Based, Learn Once, Write Anywhere.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: <>Vue</>,
+    imageUrl: 'img/vue.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The Progressive JavaScript Framework. An approachable, performant and versatile framework for building web user interfaces.
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: <>Flutter</>,
+    imageUrl: 'img/flutter.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Flutter is an open source framework by Google for building beautiful, natively compiled, multi-platform applications from a single codebase.
       </>
     ),
   },
